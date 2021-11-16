09235566c9bc0a2fe5c2cd2ffa0d24f7a3fc5603
Christian Gutierrez <chcgutierrezm@gmail.com>
2021-11-15 20:29:59 -0500

    Funcion Editar
    Se crea la funcionalidad para editar pasando los datos del producto al
    form correspondiente

--- src/main/java/com/sts/comercio/controlador/ProductoControlador.java
+++ src/main/java/com/sts/comercio/controlador/ProductoControlador.java
@@ -1,11 +1,14 @@
 package com.sts.comercio.controlador;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,7 +21,7 @@ import com.sts.comercio.servicio.ProductoServicio;
 public class ProductoControlador {
 
 	private final Logger oLogger = LoggerFactory.getLogger(ProductoControlador.class);
-	
+
 	@Autowired
 	private ProductoServicio oProduServicio;
 
@@ -42,19 +45,33 @@ public class ProductoControlador {
 	@PostMapping("/guardar")
 	public String Guardar(Producto oProducto) {
 		oLogger.info("Objeto producto enviado desde el form: {}", oProducto);
-		Usuario oUsuario=new Usuario(1, "", "", "", "", "", "", "", "");
+		Usuario oUsuario = new Usuario(1, "", "", "", "", "", "", "", "");
 		oProducto.setUsuario(oUsuario);
 		oProduServicio.GuardarProducto(oProducto);
 		return "redirect:/productos";
 	}
 
-	// Bloque para realizar la creacion del producto
-	// Se llama el form para hacer la creacion
-	@GetMapping("/editar")
-	public String EditarProductos() {
+	// Bloque para realizar la modificacion del producto
+	// Se llama el form para hacer la edicion pasando los datos del producto que se
+	// va a modificar
+	@GetMapping("/editar/{idProducto}")
+	public String EditarProductos(@PathVariable Integer idProducto, Model oModelo) {
 
+		Producto oProducto = new Producto();
+		Optional<Producto> optProducto = oProduServicio.BuscarProduId(idProducto);
+		oProducto = optProducto.get();
+		oModelo.addAttribute("producto_edit", oProducto);
+		oLogger.info("Objeto recuperado desde el form: {}", oProducto);
 		return "producto/edit";
 
+	}
+
+	@PostMapping("/modificar")
+	public String Modificar(Producto oProducto) {
+
+		oProduServicio.ActualizarProducto(oProducto);
+		return "redirect:/productos";
+		
 	}
 
 }
