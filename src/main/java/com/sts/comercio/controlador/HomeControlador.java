35762106fbe936e1671d2908f397dc0f9907973b
Christian Gutierrez <chcgutierrezm@gmail.com>
2021-11-29 11:48:25 -0500

    Vista Usuario/Producto
    Se implementa la funcionalidad para mostrar el detalle del producto al
    usuario

--- src/main/java/com/sts/comercio/controlador/HomeControlador.java
+++ src/main/java/com/sts/comercio/controlador/HomeControlador.java
@@ -1,9 +1,12 @@
 package com.sts.comercio.controlador;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sts.comercio.servicio.ProductoServicio;
@@ -11,6 +14,8 @@ import com.sts.comercio.servicio.ProductoServicio;
 @Controller
 @RequestMapping("/")
 public class HomeControlador {
+	
+	private final Logger oLogger = LoggerFactory.getLogger(HomeControlador.class);
 
 	@Autowired
 	private ProductoServicio oProduService;
@@ -21,6 +26,15 @@ public class HomeControlador {
 		oModel.addAttribute("productos_home", oProduService.MostrarTodo());
 
 		return "usuario/home";
+
+	}
+	
+	@GetMapping("producto_home/{idProducto}")
+	public String HomeProducto(@PathVariable Integer idProducto) {
+
+		oLogger.info("ID producto enviado como parámetro: {}", idProducto);
+
+		return "usuario/producto_home";
 
 	}
 
