6065ff53b22983158aaa824c147a73730c3257ad
Christian Gutierrez <chcgutierrezm@gmail.com>
2021-11-29 12:51:22 -0500

    Redirect Carrito
    Se agrega la re-direccion a la pantalla del carrito de compras

--- src/main/java/com/sts/comercio/controlador/HomeControlador.java
+++ src/main/java/com/sts/comercio/controlador/HomeControlador.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sts.comercio.modelo.Producto;
@@ -42,6 +43,15 @@ public class HomeControlador {
 		oLogger.info("Objeto recuperado desde el form: {}", oProducto);
 		
 		return "usuario/producto_home";
+
+	}
+	
+	@PostMapping("/carrito")
+	public String AgregarCarrito(Model oModel) {
+
+		oModel.addAttribute("productos_home", oProduService.MostrarTodo());
+
+		return "usuario/carrito";
 
 	}
 
