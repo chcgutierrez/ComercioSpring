31baea4bb73bd11d342d4870817e724c0e397d76
Christian Gutierrez <chcgutierrezm@gmail.com>
2021-11-29 18:58:10 -0500

    Carrito
    Se agrega la funcionalidad para el envio de los productos al carrito de
    compras

--- src/main/java/com/sts/comercio/controlador/HomeControlador.java
+++ src/main/java/com/sts/comercio/controlador/HomeControlador.java
@@ -1,5 +1,7 @@
 package com.sts.comercio.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,15 +13,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sts.comercio.modelo.DetaOrden;
+import com.sts.comercio.modelo.Orden;
 import com.sts.comercio.modelo.Producto;
 import com.sts.comercio.servicio.ProductoServicio;
 
 @Controller
 @RequestMapping("/")
 public class HomeControlador {
-	
+
 	private final Logger oLogger = LoggerFactory.getLogger(HomeControlador.class);
+
+	// Recoge el detalle de la orden
+	List<DetaOrden> mDetalleOrd = new ArrayList<DetaOrden>();
+
+	// Recoge el encabezado de la orden
+	Orden mCabeOrden = new Orden();
 
 	@Autowired
 	private ProductoServicio oProduService;
@@ -32,7 +43,7 @@ public class HomeControlador {
 		return "usuario/home";
 
 	}
-	
+
 	@GetMapping("producto_home/{idProducto}")
 	public String HomeProducto(@PathVariable Integer idProducto, Model oModelo) {
 
@@ -41,15 +52,40 @@ public class HomeControlador {
 		oProducto = optProducto.get();
 		oModelo.addAttribute("producto_home", oProducto);
 		oLogger.info("Objeto recuperado desde el form: {}", oProducto);
-		
+
 		return "usuario/producto_home";
 
 	}
-	
-	@PostMapping("/carrito")
-	public String AgregarCarrito(Model oModel) {
 
-		oModel.addAttribute("productos_home", oProduService.MostrarTodo());
+	@PostMapping("/carrito")
+	public String AgregarCarrito(@RequestParam Integer idProducto, @RequestParam Integer cantproducto, Model oModelo) {
+
+		DetaOrden detOrden = new DetaOrden();
+		Producto detProducto = new Producto();
+		Optional<Producto> optProducto = oProduService.BuscarProducto(idProducto);
+		double sumaTotal = 0;
+		double totalProdu = 0;
+
+		detProducto = optProducto.get();
+
+		totalProdu = (detProducto.getValor() * cantproducto);
+
+		detOrden.setNombre(detProducto.getNombre());
+		detOrden.setPrecio(detProducto.getValor());
+		detOrden.setCantidad(cantproducto);
+		detOrden.setTotal(totalProdu);
+		detOrden.setProducto(detProducto);
+
+		mDetalleOrd.add(detOrden);
+		
+		sumaTotal = mDetalleOrd.stream().mapToDouble(x -> x.getTotal()).sum();
+		mCabeOrden.setTotal(sumaTotal);
+		
+		oModelo.addAttribute("det_ord_carro", mDetalleOrd);
+		oModelo.addAttribute("orden_carro", mCabeOrden);
+		
+		oLogger.info("Detalle Orden: {}", mDetalleOrd);
+		oLogger.info("Cabecera Orden: {}", mCabeOrden);
 
 		return "usuario/carrito";
 
